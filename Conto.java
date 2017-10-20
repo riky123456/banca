1035466381a0023d51d4e9df9bdabed505d4777b
riky123456 <32876084+riky123456@users.noreply.github.com>
2017-10-20 12:06:16 +0200

    file addati in formato java

---
+++ Conto.java
@@ -0,0 +1,46 @@
+ public class Conto {
+
+        private int iban;
+        private String cf;
+        private double saldo;
+        private double prel;
+        private double dep;
+
+
+
+
+
+        public Conto(int iban, String cf, double s, double p, double d) {
+            this.iban = iban;
+            this.cf = cf;
+            saldo = s;
+            prel = p;
+            dep = d;
+        }
+
+
+        public double getPrel() {
+            return prel;
+        }
+
+        public double getDep() {
+            return dep;
+        }
+
+
+
+        public double Prelievo() {
+
+
+
+            return saldo - prel;
+
+        }
+
+        public double Deposito () {          /*int i, double d, double s*/
+
+
+            return saldo + dep;
+        }
+    }
+
